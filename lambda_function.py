ed55ace885859a848cf31dfb66b1e146ddef1a7f
Miguel <ca220@hotmail.com>
2021-10-20 18:05:04 -0500

    Create builspec v3

--- lambda_function.py
+++ lambda_function.py
@@ -13,6 +13,7 @@
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
 #
+#prueba 1 pipeline
 
 import greengrasssdk
 import json
