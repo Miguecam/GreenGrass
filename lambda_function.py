a3755f1ac98e277e786ee6e2e5427ecb3a4f1c1d
Miguel <ca220@hotmail.com>
2021-10-21 16:57:13 -0500

    SonarQube 3.4 AWS

--- lambda_function.py
+++ lambda_function.py
@@ -13,7 +13,7 @@
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
 #
-#prueba 3.2 pipeline Sonar
+#prueba 3.4 pipeline Sonar
 
 import greengrasssdk
 import json
